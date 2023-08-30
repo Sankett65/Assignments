934545c17bb188e6639cf97b888fb89d028e0d67
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-08-30 16:54:31 +0530

    Assignment day 1

---
+++ Daily_Assignment/src/com/assignment/assignment_day1/CommandLine.java
@@ -0,0 +1,11 @@
+package com.assignment.assignment_day1;
+
+import java.util.Scanner;
+
+public class CommandLine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter you name: ");
+        String s = sc.nextLine();
+    }
+}
---
+++ Daily_Assignment/src/com/assignment/assignment_day1/Display.java
@@ -0,0 +1,7 @@
+package com.assignment.assignment_day1;
+
+public class Display {
+    public static void main(String[] args) {
+        System.out.println("Hi I Am Sanket");
+    }
+}
